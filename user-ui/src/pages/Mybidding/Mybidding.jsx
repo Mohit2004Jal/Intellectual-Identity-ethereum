ee25c1e0624b5884aec4c40e436e007c0f40736a
greedy-wudpeckr <mudituiet@gmail.com>
2025-01-09 23:47:43 +0530

    ui changes

--- user-ui/src/pages/Mybidding/Mybidding.jsx
+++ user-ui/src/pages/Mybidding/Mybidding.jsx
@@ -5,12 +5,21 @@ import '../../css/App.css';
 import { useNavigate } from "react-router-dom";
 import '../../css/Style.css';
 import {Bidtable, MyBidstable, Loader} from '../../components/index';
+import TypingAnimation from '../../components/magicui/typing_animation';
+import PlaceholdersAndVanishInput from '../../components/magicui/input_new';
 
 const Mybidding = () => {
   const { connectWallet, currentAccount, countbids } = useContext(TransactionContext);
   const { bidsData, getMyBids } = useContext(BidderContext);
 
   const[query, setQuery] = useState("");
+  const placeholders = [
+    "Search for your active bids...",
+    "Find out which items you're bidding on...",
+    "Looking for your latest bids? Start typing...",
+    "Discover your highest bids in the auction...",
+    "Check the status of your recent bids here...",
+  ];
 
   const keys = ["ownerIPname"]
 
@@ -33,43 +42,52 @@ const Mybidding = () => {
   }
 
   return (
-    <>
-     <div className='text-center'>
-        <button
-            data-testid="wallet"
-            onClick={connectWallet}
-            className='bg-gradient-to-r from-black via-gray-500 to-black transition duration-150 ease-out hover:ease-in
-            p-4 px-6 rounded-full text-white text-xl mt-36 mb-10 hover:brightness-125 transition duration-150 ease-in-out shadow-lg'>
-            Connect Wallet
-        </button>
+    <div className='bg-black'>
+     <div className='text-center '>
+ <button 
+ data-testid="wallet"
+ onClick={connectWallet}
+ className="relative mt-12 mb-10 inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+  <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+  <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-black px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+    Connect Wallet
+  </span>
+</button>
       </div>
 
-      <div className='bg-white'>
-         <p className='text-center text-gray-400 mb-16'>connect to your wallet, to see your IP bidders'</p>
+      <div className='bg-black'>
+         <TypingAnimation className='text-center'>connect to your wallet, to see your IP bidders</TypingAnimation>
       </div>
 
       <div>
-         <a className="arrow mb-4 bg-gradient-to-r from-black via-gray-300" onClick={routeChange}>Back</a>
+      <button 
+      data-testid="wallet"
+      onClick={routeChange}
+      className="relative ml-14 mt-12 mb-10 inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+  <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+  <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-black px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+    &nbsp; Back &nbsp;
+  </span>
+</button>
       </div>
 
       <div className='flex justify-around'>
-      <p className='mx-4 py-4 text-4xl cursor-pointer'>Bids</p>
+      <p className='mx-4 py-4 text-4xl text-white cursor-pointer'>Bids</p>
       <div>
-        <p>Total bids:</p>
+        <p className='text-white'>Total bids:</p>
       </div>
       </div>
       
-      <div>
-      <input 
-        type="text"
-        placeholder="Search..."
-        className="look border-solid border-1 mb-12 mt-10 border-gray-300 mx-96 py-3 px-4"
-        onChange={(e) => setQuery(e.target.value)}
-      />
+      <div className='flex justify-center'>
+      <PlaceholdersAndVanishInput
+      className="border-1 mb-12 mt-10 mx-96 "
+      placeholders={placeholders}
+      onChange={(e) => setQuery(e.target.value)}/>
      </div>
-      {bidsData.length == 0 ? <p className='text-center'>You have not made any bids, yet</p>:null}
-      {bidsData.length != 0 ? <MyBidstable data={search(bidsData)}/> : <Loader/>}
-    </>
+     <div className='flex justify-center'>
+      <div>{bidsData.length == 0 ? <p className='text-center ml-28 mt-8 text-white'>You have no bidders, yet</p>:null}</div>
+      <div className='mt-14'>{bidsData.length != 0 ? <Bidtable data={search(bidsData)}/> : <Loader/>}</div></div>
+    </div>
   )
 }
 
--- user-ui/tailwind.config.js
+++ user-ui/tailwind.config.js
@@ -91,6 +91,25 @@ module.exports = {
             backgroundPosition: "var(--bg-size) 0",
           },
         },
+        "spin-around": {
+          "0%": {
+            transform: "translateZ(0) rotate(0)",
+          },
+          "15%, 35%": {
+            transform: "translateZ(0) rotate(90deg)",
+          },
+          "65%, 85%": {
+            transform: "translateZ(0) rotate(270deg)",
+          },
+          "100%": {
+            transform: "translateZ(0) rotate(360deg)",
+          },
+        },
+        "shimmer-slide": {
+          to: {
+            transform: "translate(calc(100cqw - 100%), 0)",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
@@ -98,6 +117,9 @@ module.exports = {
         "shiny-text": "shiny-text 8s infinite",
         "rainbow": "rainbow var(--speed, 2s) infinite linear",
         "gradient": "gradient 8s linear infinite",
+        "shimmer-slide":
+          "shimmer-slide var(--speed) ease-in-out infinite alternate",
+        "spin-around": "spin-around calc(var(--speed) * 2) infinite linear",
       },
     },
   },
